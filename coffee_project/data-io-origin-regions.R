7ee012428bce2eb04af9bbc50906d11ec9ae2844
Matheus Alves de Andrade <matheus.alves_prs@outlook.com>
2024-01-15 09:18:38 -0300

    coffee project update
    	feito processo de i/o dos dados do BR-DWGD para as regiões de origem em nível temporal diário
    	dados diários para cada região de origem em .parquet

--- coffee_project/data-io-origin-regions.R
+++ coffee_project/data-io-origin-regions.R
@@ -33,44 +33,55 @@ geo_loc_areas_origem_municipal <-
 ### Extraction from raster ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 tmin_df <-
-  raster::extract(tmin_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  na.rm = TRUE) %>%
-  gather(., key = 'DATE', value = 'TMIN',-ID)
+  raster::extract(
+    tmin_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'TMIN', -ID)
 
 tmax_df <-
-  raster::extract(tmax_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  na.rm = TRUE) %>%
-  gather(., key = 'DATE', value = 'TMAX',-ID)
+  raster::extract(
+    tmax_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'TMAX', -ID)
 
 prec_df <-
-  raster::extract(prec_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  na.rm = TRUE) %>%
-  gather(., key = 'DATE', value = 'PREC',-ID)
+  raster::extract(
+    prec_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'PREC', -ID)
 
 ## Joins & Export ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 climatology_areas_origem_regional <-
   dplyr::left_join(tmin_df, tmax_df, by = c('DATE', 'ID')) %>%
-  left_join(., prec_df, by = c('DATE', 'ID')) %>% mutate(
-    DATE = str_replace(DATE, "X", ""),
-    DATE = as_date(DATE),
-    ID = as.character(ID))
+  left_join(., prec_df, by = c('DATE', 'ID')) %>% mutate(DATE = str_replace(DATE, "X", ""),
+                                                         DATE = as_date(DATE),
+                                                         ID = as.character(ID))
 
 geo_loc_areas_origem@data$ID <- rownames(geo_loc_areas_origem@data)
 
-climatology_areas_origem_regional <- dplyr::left_join(climatology_areas_origem_regional, geo_loc_areas_origem@data, by="ID") %>% 
-  dplyr::select(REGIAO_ORIGEM, UF, everything(), -ID)
+climatology_areas_origem_regional <-
+  dplyr::left_join(climatology_areas_origem_regional,
+                   geo_loc_areas_origem@data,
+                   by = "ID") %>%
+  dplyr::select(REGIAO_ORIGEM, UF, everything(),-ID)
 
-write_csv(climatology_areas_origem_regional, "data/coffee-project/climatology_or_regional_yearly.csv")
+write_csv(
+  climatology_areas_origem_regional,
+  "data/coffee-project/climatology_or_regional_yearly.csv"
+)
 
 rm(prec_df, tmin_df, tmax_df)
 
@@ -79,32 +90,40 @@ rm(prec_df, tmin_df, tmax_df)
 ### Extraction from raster -----------------------------------------------------------------------------------------------------------------------------------
 
 tmin_df <- exact_extract(tmin_brick, geo_loc_areas_origem_municipal,
-                           include_cols=TRUE) %>% do.call('rbind', .) %>%
-  pivot_longer(cols=11:70, names_to = 'DATE', values_to = 'TMIN') %>%
+                         include_cols = TRUE) %>% do.call('rbind', .) %>%
+  pivot_longer(cols = 11:70,
+               names_to = 'DATE',
+               values_to = 'TMIN') %>%
   distinct(NOME, DATE, .keep_all = T) %>% dplyr::select(NOME, UF, REGIAO_ORIGEM, DATE, TMIN)
 
 tmax_df <- exact_extract(tmax_brick, geo_loc_areas_origem_municipal,
-                         include_cols=TRUE) %>% do.call('rbind', .) %>%
-  pivot_longer(cols=11:70, names_to = 'DATE', values_to = 'TMAX') %>%
+                         include_cols = TRUE) %>% do.call('rbind', .) %>%
+  pivot_longer(cols = 11:70,
+               names_to = 'DATE',
+               values_to = 'TMAX') %>%
   distinct(NOME, DATE, .keep_all = T) %>% dplyr::select(NOME, UF, REGIAO_ORIGEM, DATE, TMAX)
 
 prec_df <- exact_extract(prec_brick, geo_loc_areas_origem_municipal,
-                         include_cols=TRUE) %>% do.call('rbind', .) %>%
-  pivot_longer(cols=11:70, names_to = 'DATE', values_to = 'PREC') %>%
+                         include_cols = TRUE) %>% do.call('rbind', .) %>%
+  pivot_longer(cols = 11:70,
+               names_to = 'DATE',
+               values_to = 'PREC') %>%
   distinct(NOME, DATE, .keep_all = T) %>% dplyr::select(NOME, UF, REGIAO_ORIGEM, DATE, PREC)
 
 ### Joins & Export -----------------------------------------------------------------------------------------------
 
 climatology_areas_origem_municipal <-
   dplyr::left_join(tmin_df, tmax_df, by = c('NOME', 'UF', 'REGIAO_ORIGEM', 'DATE')) %>%
-  left_join(., prec_df, by = c('NOME', 'UF', 'REGIAO_ORIGEM', 'DATE')) %>% mutate(
-    DATE = str_replace(DATE, "X", ""),
-    DATE = as_date(DATE)) %>% 
+  left_join(., prec_df, by = c('NOME', 'UF', 'REGIAO_ORIGEM', 'DATE')) %>% mutate(DATE = str_replace(DATE, "X", ""),
+                                                                                  DATE = as_date(DATE)) %>%
   relocate(UF, .after = REGIAO_ORIGEM) %>% rename(MUNICIPIO = NOME) %>% arrange(REGIAO_ORIGEM)
 
 rm(tmin_df, tmax_df, prec_df)
 
-write_csv(climatology_areas_origem_municipal, "data/coffee-project/climatology_or_municipal_yearly.csv")
+write_csv(
+  climatology_areas_origem_municipal,
+  "data/coffee-project/climatology_or_municipal_yearly.csv"
+)
 
 
 # Daily Data ----------------------------------------------------------------------------------------------------------------------
@@ -114,11 +133,6 @@ ncdf_tmax <-
   'data/climate_grids/BR-DWGD/raw_data/Tmax_19610101_20200731_BR-DWGD.nc'
 ncdf_prec <-
   'data/climate_grids/BR-DWGD/raw_data/pr_19610101_20200731_BR-DWGD.nc'
-
-
-
-
-unique(geo_loc_areas_origem@data$UF)
 
 geo_loc_areas_origem <-
   geojson_read("data/pam/output/cafe/pam_cafe_geo_areas_origem.geojson",
@@ -130,12 +144,14 @@ geo_loc_areas_origem <-
 tmin_brick <- brick(ncdf_tmin, varname = 'Tmin')
 
 tmin_df <-
-  raster::extract(tmin_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  verbose=TRUE) %>%
-  gather(., key = 'DATE', value = 'TMIN',-ID)
+  raster::extract(
+    tmin_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'TMIN', -ID)
 
 rm(tmin_brick)
 
@@ -143,44 +159,55 @@ tmax_brick <- brick(ncdf_tmax, varname = 'Tmax')
 
 
 tmax_df <-
-  raster::extract(tmax_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  na.rm = TRUE) %>%
-  gather(., key = 'DATE', value = 'TMAX',-ID)
+  raster::extract(
+    tmax_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'TMAX', -ID)
 
 rm(tmax_brick)
 
 prec_brick <- brick(ncdf_prec, varname = 'pr')
 
 prec_df <-
-  raster::extract(prec_brick,
-                  geo_loc_areas_origem,
-                  df = TRUE,
-                  fun = mean,
-                  na.rm = TRUE) %>%
-  gather(., key = 'DATE', value = 'PREC',-ID)
+  raster::extract(
+    prec_brick,
+    geo_loc_areas_origem,
+    df = TRUE,
+    fun = mean,
+    na.rm = TRUE
+  ) %>%
+  gather(., key = 'DATE', value = 'PREC', -ID)
 
 ## Joins, manipulations and output ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 climatology_areas_origem_regional <-
   dplyr::left_join(tmin_df, tmax_df, by = c('DATE', 'ID')) %>%
-  left_join(., prec_df, by = c('DATE', 'ID')) %>% mutate(
-    DATE = str_replace(DATE, "X", ""),
-    DATE = as_date(DATE),
-    ID = as.character(ID))
+  left_join(., prec_df, by = c('DATE', 'ID')) %>% mutate(DATE = str_replace(DATE, "X", ""),
+                                                         DATE = as_date(DATE),
+                                                         ID = as.character(ID))
 
 rm(prec_df, tmin_df, tmax_df)
 
 geo_loc_areas_origem@data$ID <- rownames(geo_loc_areas_origem@data)
 
-climatology_areas_origem_regional <- dplyr::left_join(climatology_areas_origem_regional, geo_loc_areas_origem@data, by="ID") %>% 
-  dplyr::select(REGIAO_ORIGEM, UF, everything(), -ID)
+climatology_areas_origem_regional <-
+  dplyr::left_join(climatology_areas_origem_regional,
+                   geo_loc_areas_origem@data,
+                   by = "ID") %>%
+  dplyr::select(REGIAO_ORIGEM, UF, everything(),-ID)
 
-climatology_areas_origem_regional <- climatology_areas_origem_regional %>% mutate(TMIN = na_if(TMIN, NaN),
-                                                     TMAX = na_if(TMAX, NaN),
-                                                     PREC = na_if(PREC, NaN))
+climatology_areas_origem_regional <-
+  climatology_areas_origem_regional %>% mutate(
+    TMIN = na_if(TMIN, NaN),
+    TMAX = na_if(TMAX, NaN),
+    PREC = na_if(PREC, NaN)
+  )
 
-write_parquet(climatology_areas_origem_regional, "data/coffee-project/climatology_or_regional_daily.parquet")
-
+write_parquet(
+  climatology_areas_origem_regional,
+  "data/coffee-project/climatology_or_regional_daily.parquet"
+)
